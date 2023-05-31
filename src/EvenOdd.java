934e22ba6a5a734054ee481a8d73277e10989cd6
McStudent01 <mcmillcy@mail.uc.edu>
2023-05-31 10:43:21 -0500

    final commit

--- src/EvenOdd.java
+++ src/EvenOdd.java
@@ -2,6 +2,12 @@ public class EvenOdd
 {
     public static void main(String[] args)
     {
+        int numToExam = 22;
 
+        if (numToExam % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
     }
 }
